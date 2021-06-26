e53e5aed3455411bc3afcef08eb2f2e395ce66a1
Glauber <glauberss2007@gmail.com>
2021-06-26 10:08:04 -0300

    Implemented base project

---
+++ src/main/java/com/beerstockapi/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.beerstockapi.mapper;
+
+import com.beerstockapi.dto.BeerDTO;
+import com.beerstockapi.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
---
+++ src/main/java/com/beerstockapi/repository/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.beerstockapi.repository;
+
+import com.beerstockapi.entity.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
