8876f34564cf58c91c26ec323101ade997e9ab8d
Glauber <glauberss2007@gmail.com>
2021-06-26 09:26:14 -0300

    Starting from Spring Boot Project

---
+++ src/test/java/com/beerstockapi/BeerStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
